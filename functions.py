a179601eff15fd9c9fc94c1bb990189c80521667
elyasaf <elyasafw1997@gmail.com>
2026-02-02 15:25:19 +0200

    added func square circle nad update fns

--- functions.py
+++ functions.py
@@ -1,22 +1,22 @@
 #function add num 1 +num 2
 def add(num1, num2):
-    if type(num1) == int and type(num2) == int:
+    if isinstance(num1, (int, float)) and isinstance(num2, (int, float)):
         return num1 + num2
     else:
-         return "Please enter numbers only"
+         return "You can only perform arithmetic operations on numbers!"
 
       
 # This is a function that accepts 2 numbers and returns their subtraction.
 def func_sub(num1, num2):
-    try:
-        return num1-num2
-    except ValueError,TypeError:
-        return "Please select a number"
+    if isinstance(num1, (int, float)) and isinstance(num2, (int, float)):
+        return num1 - num2
+    else:
+        return "You can only perform arithmetic operations on numbers!"
     
     
 # A function that performs a multiplication operation between 2 numbers
 def multiply(num1, num2):
-    if type(num1) == int and type(num2) == int:
+    if isinstance(num1, (int, float)) and isinstance(num2, (int, float)):
         return num1 * num2
     else:
         return "You can only perform arithmetic operations on numbers!"
@@ -24,9 +24,19 @@ def multiply(num1, num2):
 
 # Function that calculates the area of ​​a square and a rectangle
 def square_area(Length, width):
-    if type(Length) == int and type(width) == int:
-        return Length * width
-    elif Length <= 0 or width <= 0:
-        return "There is no area size with value 0.."
-    else:
+    if not (isinstance(Length, (int, float)) and isinstance(width, (int, float))):
         return "You can only perform area calculations on numbers!"
+    if Length <= 0 or width <= 0:
+        return "There is no area size with value 0 or less.."
+    return Length * width
+
+    
+
+# A function that accepts a radius array and returns the circumference of a circle
+def square_circle(radius):
+    if not isinstance(radius, (int, float)):
+        return "Only numbers can be calculated."
+    if radius <= 0:
+        return "Cannot calculate 0 or negative radius.."
+    pi_approx = 3.14159
+    return pi_approx * (radius ** 2)
